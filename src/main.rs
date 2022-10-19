cfdb89500331e2827ba14658115515c8e11b5083
Kit Matthewson <kit.matthewson@gmail.com>
2022-10-19 18:18:32 +0100

    Improve UI and format

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use macroquad::{miniquad::conf::Platform, prelude::*};
 
-mod utils;
 mod shape;
 mod tool;
+mod utils;
 
 use crate::shape::*;
 use crate::tool::*;
@@ -49,17 +49,52 @@ async fn main() {
         purple: Color::from_rgba(177, 98, 134, 255),
     };
 
+    let mut show_interface = true;
+
     let mut shapes: Vec<Shape> = Vec::new();
     let mut intersections: Vec<Vec2> = Vec::new();
     let mut points: Vec<Vec2> = Vec::new();
 
-    let colours = vec![pallette.white, pallette.gray, pallette.red, pallette.green, pallette.yellow, pallette.blue, pallette.purple];
+    let colours = vec![
+        pallette.white,
+        pallette.gray,
+        pallette.red,
+        pallette.green,
+        pallette.yellow,
+        pallette.blue,
+        pallette.purple,
+    ];
+
     let mut current_colour = 0;
 
     let tools: Vec<&dyn Tool> = vec![&Compass, &StraightEdge];
     let mut current_tool = 0;
 
     let snap_radius = 15.0;
+
+    add_shape(
+        Shape::Line {
+            points: [
+                Vec2::new(-1.0, screen_height() / 2.0),
+                Vec2::new(1.0, screen_height() / 2.0),
+            ],
+            colour: pallette.white,
+        },
+        &mut shapes,
+        &mut intersections,
+    );
+
+    add_shape(
+        Shape::Line {
+            points: [
+                Vec2::new(screen_width() / 2.0, -1.0),
+                Vec2::new(screen_width() / 2.0, 1.0),
+            ],
+            colour: pallette.white,
+        },
+        &mut shapes,
+        &mut intersections,
+    );
 
     loop {
         clear_background(pallette.black);
@@ -69,8 +104,6 @@ async fn main() {
 
         let mut distance = f32::MAX;
         for intersection in intersections.iter() {
-            utils::draw_circle(*intersection, 2.0, pallette.red);
-
             let sqr_dist_to_mouse = intersection.distance_squared(mouse);
             if sqr_dist_to_mouse < snap_radius * snap_radius && sqr_dist_to_mouse < distance {
                 distance = sqr_dist_to_mouse;
@@ -78,7 +111,15 @@ async fn main() {
             }
         }
 
-        draw_line(mouse.x, mouse.y, mouse_position().0, mouse_position().1, 1.0, pallette.gray);
+        draw_line(
+            mouse.x,
+            mouse.y,
+            mouse_position().0,
+            mouse_position().1,
+            1.0,
+            pallette.gray,
+        );
+
         utils::draw_circle(mouse, 2.0, pallette.yellow);
 
         for point in points.iter() {
@@ -93,6 +134,10 @@ async fn main() {
 
         if is_key_pressed(KeyCode::Tab) {
             current_tool = (current_tool + 1) % tools.len();
+        }
+
+        if is_key_pressed(KeyCode::F11) {
+            show_interface = !show_interface;
         }
 
         if mouse_wheel().1 < 0.0 {
@@ -118,26 +163,44 @@ async fn main() {
 
         if points.len() == tools[current_tool].num_points() as usize {
             let shape = tools[current_tool].get_shape(&points, colours[current_colour]);
-
-            for b in &shapes {
-                intersections.append(find_intersection(&shape, b).as_mut());
-            }
-
-            shapes.push(shape);
+            add_shape(shape, &mut shapes, &mut intersections);
             points.clear();
         } else if points.len() > 0 {
-            tools[current_tool].draw_guide(&points, mouse, set_opacity(colours[current_colour], 0.4));
+            tools[current_tool].draw_guide(
+                &points,
+                mouse,
+                set_opacity(colours[current_colour], 0.4),
+            );
         }
 
         draw_shapes(&shapes);
-        draw_interface(font, &pallette, &tools, current_tool, &colours, current_colour);
+
+        if show_interface {
+            draw_interface(
+                font,
+                &pallette,
+                &tools,
+                current_tool,
+                &colours,
+                current_colour,
+            );
+        }
 
         next_frame().await
     }
 }
 
-fn set_opacity(colour: Color, a: f32) -> Color {
-    Color::new(colour.r, colour.g, colour.b, a)
+fn add_shape(shape: Shape, shapes: &mut Vec<Shape>, intersections: &mut Vec<Vec2>) {
+    for b in shapes.iter() {
+        intersections.append(find_intersection(&shape, b).as_mut());
+    }
+
+    for int in intersections.iter() {
+        println!("{}, {}", screen_width() / 2.0, screen_height() / 2.0);
+        println!("{}, {}", int.x, int.y);
+    }
+
+    shapes.push(shape);
 }
 
 fn draw_shapes(shapes: &Vec<Shape>) {
@@ -148,7 +211,8 @@ fn draw_shapes(shapes: &Vec<Shape>) {
             }
 
             Shape::Line {
-                points: [pos1, pos2], colour,
+                points: [pos1, pos2],
+                colour,
             } => utils::draw_line(*pos1, *pos2, *colour),
 
             _ => (),
@@ -156,13 +220,29 @@ fn draw_shapes(shapes: &Vec<Shape>) {
     }
 }
 
-fn draw_interface(font: Font, pallette: &ColourPalette, tools: &Vec<&dyn Tool>, selected_tool: usize, colours: &Vec<Color>, selected_colour: usize) {
-    let padding = 8.0;
+fn draw_interface(
+    font: Font,
+    pallette: &ColourPalette,
+    tools: &Vec<&dyn Tool>,
+    selected_tool: usize,
+    colours: &Vec<Color>,
+    selected_colour: usize,
+) {
+    let padding = 16.0;
     let font_size = 12.0;
+
+    draw_text(
+        "Euclid Geometry Engine",
+        padding,
+        padding + font_size,
+        font_size as u16,
+        font,
+        pallette.white,
+    );
 
     for (i, tool) in tools.iter().enumerate() {
         let x = padding;
-        let y = 30.0 + (font_size * (i as f32));
+        let y = padding + ((font_size + 2.0) * (i as f32 + 2.0));
 
         let text = if i == selected_tool {
             format!("> {}", tool.name())
@@ -176,7 +256,8 @@ fn draw_interface(font: Font, pallette: &ColourPalette, tools: &Vec<&dyn Tool>, 
     let radius = 8.0;
 
     for (i, colour) in colours.iter().enumerate() {
-        let x = screen_width() / 2.0 + (i as f32 - (colours.len() as f32 / 2.0)) * (radius * 2.0 + padding);
+        let x = screen_width() / 2.0
+            + (i as f32 - (colours.len() as f32 / 2.0)) * (radius * 2.0 + padding);
         let mut y = screen_height() - padding - radius - radius;
 
         if i == selected_colour {
@@ -185,33 +266,10 @@ fn draw_interface(font: Font, pallette: &ColourPalette, tools: &Vec<&dyn Tool>, 
 
         draw_circle(x, y, radius, *colour);
     }
-
-    draw_centred_text(
-        "Euclid Geometry Engine",
-        screen_width() / 2.0,
-        padding + 8.0,
-        16,
-        font,
-        pallette.white,
-    );
 }
 
-fn draw_centred_text(text: &str, x: f32, y: f32, font_size: u16, font: Font, colour: Color) -> TextDimensions {
-    let dimensions = measure_text(text, Some(font), font_size, 1.0);
-
-    draw_text_ex(
-        text,
-        x - (dimensions.width / 2.0),
-        y + dimensions.height,
-        TextParams {
-            font,
-            font_size,
-            color: colour,
-            ..Default::default()
-        },
-    );
-
-    return dimensions;
+fn set_opacity(colour: Color, a: f32) -> Color {
+    Color::new(colour.r, colour.g, colour.b, a)
 }
 
 fn draw_text(text: &str, x: f32, y: f32, font_size: u16, font: Font, colour: Color) {
