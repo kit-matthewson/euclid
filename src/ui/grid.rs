9fb773db9b7af535506e369e7eecfcfbe686aae9
Kit Matthewson <kit.matthewson@gmail.com>
2023-10-26 19:36:33 +0100

    Rename text_row

--- src/ui/grid.rs
+++ src/ui/grid.rs
@@ -2,7 +2,7 @@ pub fn new(id: &str) -> egui::Grid {
     egui::Grid::new(id).num_columns(1).striped(false)
 }
 
-pub fn text_row(ui: &mut egui::Ui, key: &str, text: &str) {
+pub fn add_text_row(ui: &mut egui::Ui, key: &str, text: &str) {
     add_row(ui, key, |ui| ui.monospace(text));
 }
 
@@ -24,7 +24,7 @@ pub fn add_struct<T: serde::Serialize>(ui: &mut egui::Ui, data_struct: T) {
 
     if let serde_json::Value::Object(map) = values {
         for (k, v) in map.into_iter() {
-            text_row(ui, &clean_key(&k), clean_value(&v.to_string()));
+            add_text_row(ui, &clean_key(&k), clean_value(&v.to_string()));
         }
     } else {
         panic!("struct did not serialize to object")
